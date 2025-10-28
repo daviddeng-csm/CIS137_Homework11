ad1b6c04e704f79b0a40e975b18095211c72936b
David Deng <daviddeng@my.smccd.edu>
2025-10-28 12:35:49 -0700

    Initial Commit

---
+++ Homework11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework11
+//
+//  Created by David Deng on 10/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homework11/Homework11App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework11App.swift
+//  Homework11
+//
+//  Created by David Deng on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Homework11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
